23ff210cfb521f7c71bc10c73fcf09c02e827a14
Carlos Huamaní <carlos.hs.92@gmail.com>
2013-04-21 19:30:06 -0500

    south

--- project/project/settings.py
+++ project/project/settings.py
@@ -154,6 +154,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
+    'south',
     'grappelli',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
