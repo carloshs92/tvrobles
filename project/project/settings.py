86b593ece7b49c8ac10c543beee6dda65776b58d
Carlos Huamaní <carlos.hs.92@gmail.com>
2013-04-22 20:58:14 -0500

    joder

--- project/project/settings.py
+++ project/project/settings.py
@@ -18,31 +18,21 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'tvrobles',                      # Or path to database file if using sqlite3.
-#        'USER': 'postgres',                      # Not used with sqlite3.
-#        'PASSWORD': 'inei.2012',                  # Not used with sqlite3.passwrods
-#        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dfbg33ulku968q',                      # Or path to database file if using sqlite3.
-        'USER': 'nfbkcuwrsibgky',                      # Not used with sqlite3.
-        'PASSWORD': 'sUJd1Xfqwx9-8WNLnUF2cG4q-H',                  # Not used with sqlite3.        'HOST': 'ec2-23-23-201-251.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'tvrobles',                      # Or path to database file if using sqlite3.        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'inei.2012',                  # Not used with sqlite3.passwrods
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'portalineiperu@gmail.com'
+EMAIL_HOST_PASSWORD = 'inei.2012'
 EMAIL_USE_TLS = True
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
@@ -195,8 +185,8 @@ LOGGING = {
 }
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
